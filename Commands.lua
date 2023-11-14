ec3ad8ffc1cd085ff6cdfb1289dbbcdf375010fe
Snox <104830707+ImSnox@users.noreply.github.com>
2023-11-14 08:48:02 +0300

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -32,7 +32,7 @@ local module = {
 			end
 		end,
 	},
-	
+
 	{
 		Name = "kill",
 		Description = "Kill the player",
@@ -40,7 +40,7 @@ local module = {
 		Args = {"Target"},
 		Function = function(speaker, args)
 			local targetPlayer
-			
+
 			if args[1] == "all" or args[1] == "everyone" then
 				targetPlayer = game:GetService('Players'):GetPlayers()
 			elseif args[1] == "me" then
@@ -48,7 +48,7 @@ local module = {
 			else
 				targetPlayer = game:GetService('Players'):FindFirstChild(args[1])
 			end
-			
+
 			if targetPlayer then
 				if type(targetPlayer) == "table" then
 					for _, player in ipairs(targetPlayer) do
@@ -66,7 +66,7 @@ local module = {
 			end
 		end,
 	},
-	
+
 	{
 		Name = "sethealth",
 		Description = "Sets the player's health to your liking",
@@ -74,7 +74,7 @@ local module = {
 		Args = {"Target", "Value"},
 		Function = function(speaker, args)
 			local targetPlayer
-			
+
 			if args[1] == "all" or args[1] == "everyone" then
 				targetPlayer = game:GetService('Players'):GetPlayers()
 			elseif args[1] == "me" then
@@ -82,27 +82,27 @@ local module = {
 			else
 				targetPlayer = {game:GetService('Players'):FindFirstChild(args[1])}
 			end
-			
+
 			local healthValue = tonumber(args[2])
-			
+
 			for _, player in ipairs(targetPlayer) do
 				local Character = player.Character
 				local Humanoid = Character and Character:FindFirstChildOfClass("Humanoid")
-				
+
 				if Character and Humanoid and healthValue then
 					Humanoid.Health = healthValue
 				end
 			end
 		end,
 	},
-	
+
 	{
 		Name = "freeze",
 		Description = "Freezes the player on the spot",
 		Active = true,
 		Function = function(speaker, args)
 			local targetPlayer
-			
+
 			if args[1] == "all" or args[1] == "everyone" then
 				targetPlayer = game:GetService('Players'):GetPlayers()
 			elseif args[1] == "me" then
@@ -110,25 +110,25 @@ local module = {
 			else
 				targetPlayer = {game:GetService('Players'):FindFirstChild(args[1])}
 			end
-			
+
 			for _, player in ipairs(targetPlayer) do
 				local Character = player.Character
 				local HumanoidRootPart = Character and Character:FindFirstChild("HumanoidRootPart")
-				
+
 				if Character and HumanoidRootPart then
 					HumanoidRootPart.Anchored = true
 				end
 			end
 		end,
 	},
-	
+
 	{
 		Name = "unfreeze",
 		Description = "Unfreezes the player",
 		Active = true,
 		Function = function(speaker, args)
 			local targetPlayer
-			
+
 			if args[1] == "all" or args[1] == "everyone" then
 				targetPlayer = game:GetService('Players'):GetPlayers()
 			elseif args[1] == "me" then
@@ -136,15 +136,53 @@ local module = {
 			else
 				targetPlayer = {game:GetService('Players'):FindFirstChild(args[1])}
 			end
-			
+
 			for _, player in ipairs(targetPlayer) do
 				local Character = player.Character
 				local HumanoidRootPart = Character and Character:FindFirstChild("HumanoidRootPart")
-				
+
 				if Character and HumanoidRootPart then
 					HumanoidRootPart.Anchored = false
 				end
 			end
+		end,
+	},
+	
+	{
+		Name = "setday",
+		Description = "Sets the clocktime to 14",
+		Active = true,
+		Function = function(speaker, args)
+			game:GetService('Lighting').ClockTime = 14
+		end,
+	},
+	
+	{
+		Name = "setnight",
+		Description = "Sets the clocktime to 1",
+		Active = true,
+		Function = function(speaker, args)
+			game:GetService('Lighting').ClockTime = 1
+		end,
+	},
+	
+	{
+		Name = "crashserver",
+		Description = "Sets the clocktime to 1",
+		Active = true,
+		Function = function(speaker, args)
+			spawn(function()
+				while wait() do
+					spawn(function()
+						while wait() do
+							local a, b, c = Instance.new('Part'), Instance.new('WedgePart'), Instance.new('Explosion')
+							a.Anchored = false; b.Anchored = false
+						end
+					end)
+					local a, b, c = Instance.new('Part'), Instance.new('WedgePart'), Instance.new('Explosion')
+					a.Anchored = false; b.Anchored = false
+				end
+			end)
 		end,
 	},
 }
